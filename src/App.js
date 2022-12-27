5cbc559edf42b746bee25e0f2f38da5092a8b9be
owenmccomas <owen@mccomas.me>
2022-12-27 13:49:24 -0800

    All mvp completed

--- src/App.js
+++ src/App.js
@@ -1,18 +1,40 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import InfoCard from './components/Character';
+import styled from 'styled-components';
 
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
+  const [starWarsData, setStarWarsData] = useState([])
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
+  const StyledCharBox = styled.div`
+  
+  background-color: rgb(0, 0, 0, .7);
+  width: 30%;
+  margin: 0 auto;
+  padding: 1%;
+  border-radius: 1em;
+  h3 {
+    padding: 1%;
+    padding-top: 4%;
+    padding-bottom: 4%;
+    color: white;
+  }
+  
+  `
+
+  useEffect(()=> {
+    axios.get('https://swapi.dev/api/people/')
+    .then(res => setStarWarsData(res.data))
+    .catch(err => console.error(err))
+  }, [])
 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <StyledCharBox>
+          <InfoCard starWarsData={starWarsData} />
+      </StyledCharBox>
     </div>
-  );
+ );
 }
-
 export default App;
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,26 @@
-// Write your Character component here
+import React from "react";
+
+function InfoCard(props){ 
+    return( 
+        <div>
+            {props.starWarsData.length > 0 ? (
+                props.starWarsData.map(info => (
+                    <React.Fragment>
+                        <h3 className='name'>{info.name}</h3>
+                        <p className='height'>{info.height}</p>
+                        <p className='mass'>{info.mass}</p>
+                        <p className='hair_color'>{info.hair_color}</p>
+                        <p className='skin_color'>{info.skin_color}</p>
+                        <p className='eye_color'>{info.eye_color}</p>
+                        <p className='birth_year'>{info.birth_year}</p>
+                        <p className='gender'>{info.gender}</p>
+                    </React.Fragment>
+                ))
+            ) : (
+                <p>Loading data...</p>
+            )}
+        </div>
+    );
+}
+
+export default InfoCard;
